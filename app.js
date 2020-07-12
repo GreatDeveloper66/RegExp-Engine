d0289a33559f64e8154327cfe6b8d7d94d6df91e
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-12 12:43:02 -0500

    Upgrade app to include wildcharacter capability

--- app.js
+++ app.js
@@ -23,10 +23,13 @@ ask("Input Regular Expression Pattern\n")
 function test(pattern,text){
     let i = 0
     while(i < text.length) {
-        if(text.charAt(i) === pattern[0]){
+        if(text.charAt(i) === pattern[0] || pattern[0] === '.'){ 
             let j = 1
-            while(j < pattern.length && pattern[j] === text.charAt(i + j)){
+            while(j < pattern.length && (pattern[j] === text.charAt(i + j) || pattern[j] === '.')){
                 j += 1
+                if(i + j > text.length){
+                  return false
+                }
             }
             if(j === pattern.length){
                 return true
