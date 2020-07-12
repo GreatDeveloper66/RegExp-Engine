df0fbb2ddbe78c3544ab85f5adc81ffc59d010e2
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-12 12:59:46 -0500

    Import test.js to app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const readline = require('readline')
+const { test } = require("./test")
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -20,22 +21,3 @@ ask("Input Regular Expression Pattern\n")
    })
   .catch(error => console.log(error))
 
-function test(pattern,text){
-    let i = 0
-    while(i < text.length) {
-        if(text.charAt(i) === pattern[0] || pattern[0] === '.'){ 
-            let j = 1
-            while(j < pattern.length && (pattern[j] === text.charAt(i + j) || pattern[j] === '.')){
-                j += 1
-                if(i + j > text.length){
-                  return false
-                }
-            }
-            if(j === pattern.length){
-                return true
-            }
-        }
-        i += 1
-    }
-    return false
-}
