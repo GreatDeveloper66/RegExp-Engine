79e3e3a7977505c7101877dfb4d060d4fb7aa524
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-12 21:53:22 -0500

    Add if else logic to inner loop

--- test.js
+++ test.js
@@ -2,13 +2,22 @@ export default function test(pattern, text) {
   let i = 0;
   while (i < text.length) {
     if (text.charAt(i) === pattern[0] || pattern[0] === '.') {
-      let j = 1;
+      let j = 1
+      let k = i + j
       while (j < pattern.length && 
-            (pattern[j] === text.charAt(i + j) || 
+            (pattern[j] === text.charAt(k) || 
             pattern[j] === '.' ||
             pattern[j + 1] === '?')) {
-        (pattern[j + 1] === '?' ? j += 2 : j += 1;
-
+            if(pattern[j + 1] === '?'){
+              if(pattern[j] === text.charAt(k)){
+                j += 1
+              }
+              else {
+                
+              }
+            }
+        
+            j += 1
         if (i + j > text.length) {
           return false;
         }
