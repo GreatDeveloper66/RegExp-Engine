19f61ee0daa6ec8031bf64c73f69f21cc4873781
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-12 21:39:35 -0500

    Add ? capability to function

--- test.js
+++ test.js
@@ -3,8 +3,12 @@ export default function test(pattern, text) {
   while (i < text.length) {
     if (text.charAt(i) === pattern[0] || pattern[0] === '.') {
       let j = 1;
-      while (j < pattern.length && (pattern[j] === text.charAt(i + j) || pattern[j] === '.')) {
-        j += 1;
+      while (j < pattern.length && 
+            (pattern[j] === text.charAt(i + j) || 
+            pattern[j] === '.' ||
+            pattern[j + 1] === '?')) {
+        (pattern[j + 1] === '?' ? j += 2 : j += 1;
+
         if (i + j > text.length) {
           return false;
         }
@@ -13,6 +17,7 @@ export default function test(pattern, text) {
         return true;
       }
     }
+  
     i += 1;
   }
   return false;
