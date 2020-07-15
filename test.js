a943a57c6d568d41589aef7cb095604bcf20b64d
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-14 23:56:18 -0500

    Add ? wild character to test.js functionality

--- test.js
+++ test.js
@@ -1,34 +1,39 @@
 export default function test(pattern, text) {
-  let i = 0;
-  while (i < text.length) {
-    if (text.charAt(i) === pattern[0] || pattern[0] === '.') {
-      let j = 1
-      let k = i + j
-      while (j < pattern.length && 
-            (pattern[j] === text.charAt(k) || 
-            pattern[j] === '.' ||
-            pattern[j + 1] === '?')) {
-            if(pattern[j + 1] === '?'){
-              if(pattern[j] === text.charAt(k)){
-                j += 1
-              }
-              else {
-                
-              }
-            }
-        
-            j += 1
-        if (i + j > text.length) {
-          return false;
+  //pattern is the regular expression pattern to match against
+  //text is the text with which to find the regular expression pattern
+  //textCounter-the counter that points to the place in the text the engine is checking
+  //patternCounter-the counter that points to the place in the regular expression patternCounter
+  //being checked
+  //innerCounter-the counter of the inner loop for the current place in the text being
+  //checked against patternCounter
+  let textCounter = 0
+  while(textCounter < text.length){
+    let patternCounter = 0
+    if(text[textCounter] === pattern[0]
+      || pattern[0] === '.'
+      || pattern[patternCounter + 1] === '?'
+      ){
+      let innerCounter = textCounter
+      while(patternCounter < pattern.length &&
+        (pattern[patternCounter] === text[innerCounter]
+          || pattern[patternCounter] === '.'
+          || pattern[patternCounter + 1] === '?'
+        )){
+        if(pattern[patternCounter + 1] === '?'){
+          if(pattern[patternCounter] !== text[innerCounter]){
+            innerCounter -= 1
+          }
+          patternCounter += 1
         }
+        innerCounter += 1
+        patternCounter += 1
       }
-      if (j === pattern.length) {
-        return true;
+      if(patternCounter >= pattern.length){
+        return true
       }
     }
-  
-    i += 1;
+    textCounter += 1
   }
-  return false;
+  return false
 }
 //exports.test = test;
